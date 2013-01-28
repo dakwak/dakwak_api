23e74c9e53066d89d5c96da2dc704936725093bb
nour <nour.fwh@gmail.com>
2013-01-28 14:20:21 +0200

    add styler

--- lib/dakwak.rb
+++ lib/dakwak.rb
@@ -1,16 +1,22 @@
 require "dakwak/version"
 # require "dakwak/command"
+require "dakwak/simple_styler"
 
 module Dakwak
-  # Your code goes here...
   module Command
     def self.intro
       %Q{
-        dakwak is a website localization service that offers both machine and professional translation in 54 
-        different languages.\n 
+        dakwak is a website localization service that offers both machine and professional translation 
+        in 54 different languages.
+
         You can quickly and easily translate your websites to your languages of choice, 
-        and have them up and running in no time.\n\n
-        Start your 14 day trial now: https://dakwak.com}
+        and have them up and running in no time.
+
+        Start your 14 day trial now: https://dakwak.com
+      }
+    end
+    def self.style(text,options={:color=>:reset, :bold=>false})
+      SimpleStyler.new(text).style_text(options)
     end
   end
 end
