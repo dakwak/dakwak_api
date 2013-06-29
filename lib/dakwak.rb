ebb8f3d8fc684c77ffb6d715318f0078f66214a1
nour <nour.fwh@gmail.com>
2013-06-29 11:03:12 +0300

    change dakwak description

--- lib/dakwak.rb
+++ lib/dakwak.rb
@@ -3,13 +3,13 @@ require "dakwak/version"
 module Dakwak
   def self.intro
     %Q{
-      dakwak is a website localization service that offers both machine and professional translation 
-      in 54 different languages.
+  dakwak is a website translation and localization technology that helps businesses in going global by having up and running translated versions of their website with no technical involvement and no integration. 
 
-      You can quickly and easily translate your websites to your languages of choice, 
-      and have your translated websites up and running in no time.
+  Translation quality is user-controlled through dakwak's multi-layered translation system offering machine, team, and professional translation options.
 
-      Start your 14 day trial at dakwak now: https://dakwak.com
+  Translated versions of the website run initially under a temporary subdomain provided by dakwak till the user configures his DNS and makes those versions run under his own domain name.
+
+  Once the translated website is running under your own domain name, translated pages will start being indexed in search engines making your website visible to people searching in their native language.  
     }
   end
 end
